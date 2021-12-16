2b9eaf631950b99a1da0dd7c6e3ec070d9e871c3
Christopher <christopher.black.2209@googlemail.com>
2021-12-16 02:21:30 +0000

    Fast json added and Item serializer created

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -1,11 +1,17 @@
 class ItemsController < ApplicationController
     def index
         items = Item.all
-        render json: items
+        render json: items, except: [:created_at, :updated_at]
     end 
 
     def show
         item = Item.find_by(id: params[:id])
         render json: item
     end
+
+    # def create
+    # end
+
+    # def destroy
+    # end
 end
