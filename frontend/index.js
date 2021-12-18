0b2e0010670f81e303f7f32358562e0f752ebf8c
Christopher <christopher.black.2209@googlemail.com>
2021-12-18 17:44:40 +0000

    Status added to items

--- frontend/index.js
+++ frontend/index.js
@@ -11,14 +11,29 @@ function getItems() {
 function createItems(jsonItems) {
     const itemContainer = document.querySelector("#item-container");
     for(const item of jsonItems) {
-        let name = document.createElement("h2");
-        name.innerHTML = item.name;
-        let image = document.createElement("img");
-        image.src = item.image_url;
+        // let name = document.createElement("h2");
+        // name.innerHTML = item.name;
+        // let image = document.createElement("img");
+        // image.src = item.image_url;
+        // let itemCard = document.createElement("div");
+        // itemCard.appendChild(name);
+        // itemCard.appendChild(image);
+        // itemContainer.appendChild(itemCard)
         let itemCard = document.createElement("div");
-        itemCard.appendChild(name);
-        itemCard.appendChild(image);
-        itemContainer.appendChild(itemCard)
+        itemCard.className = "item";
+        itemCard.innerHTML = `
+            <img src=${item.image_url} height="200" width="200">
+            <h2>${item.name}<h2>
+        `;
+        if (item.lost_status === true) {
+            let status = document.createElement("h3");
+            status.innerHTML = "LOST!";
+            itemCard.appendChild(status)
+        } else if (item.found_status === true) {
+            let status = document.createElement("h3");
+            status.innerHTML = "FOUND!";
+            itemCard.appendChild(status)
+        }
+        itemContainer.appendChild(itemCard);
     }
 }
-
